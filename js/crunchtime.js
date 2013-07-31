677bf7358a680ebd58464123af9496ea2b13d574
eduardo <piscosour@gmail.com>
2013-07-30 21:44:33 -0500

    renderMechanics()

--- js/crunchtime.js
+++ js/crunchtime.js
@@ -17,6 +17,7 @@ function PlayerClass(name) {
 	this.investor = 0;
 	this.luck = 20;
 	this.stress = 50;
+	this.burnrate = 20;
 }
 
 var playerName = window.prompt("Enter player name");
@@ -34,11 +35,83 @@ function updatePlayerStats() {
 	document.getElementById("cashBox").innerHTML = "$" + player.cash;
 }
 
+// render player actions
+function renderMechanics() {
+	var mechanicsBlock = document.getElementById("mechanicsBlock");
+
+	// clear any previous HTML
+	mechanicsBlock.innerHTML = "";
+
+	// to do: move all mechanics to a JSON File
+	var tr = document.createElement("tr");
+	var td = tr.appendChild(document.createElement("td"));
+	var td2 = tr.appendChild(document.createElement("td"));
+	td.innerHTML = '<h3>Work on your prototype</h3>';
+	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsDevelop()">Crunch!</button>'
+	mechanicsBlock.appendChild(tr);
+
+	var tr = document.createElement("tr");
+	var td = tr.appendChild(document.createElement("td"));
+	var td2 = tr.appendChild(document.createElement("td"));
+	td.innerHTML = '<h3>Hire a new team member</h3>';
+	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsHire()">Crunch!</button>'
+	mechanicsBlock.appendChild(tr);
+
+	var tr = document.createElement("tr");
+	var td = tr.appendChild(document.createElement("td"));
+	var td2 = tr.appendChild(document.createElement("td"));
+	td.innerHTML = '<h3>Look for client work</h3>';
+	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsClient()">Crunch!</button>'
+	mechanicsBlock.appendChild(tr);
+
+	var tr = document.createElement("tr");
+	var td = tr.appendChild(document.createElement("td"));
+	var td2 = tr.appendChild(document.createElement("td"));
+	td.innerHTML = '<h3>Test game with players</h3>';
+	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsTesting()">Crunch!</button>'
+	mechanicsBlock.appendChild(tr);
+
+	var tr = document.createElement("tr");
+	var td = tr.appendChild(document.createElement("td"));
+	var td2 = tr.appendChild(document.createElement("td"));
+	td.innerHTML = '<h3>Take a day off</h3>';
+	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsBreak()">Crunch!</button>'
+	mechanicsBlock.appendChild(tr);
+
+	if (player.completion >= 75) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>Pitch game to investors</h3>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsPitch()">Crunch!</button>'
+		mechanicsBlock.appendChild(tr);
+	}
+
+	if (player.completion >= 85) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>Present game at a developer conference</h3>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsConference()">Crunch!</button>'
+		mechanicsBlock.appendChild(tr);
+	}
+
+	if (player.completion >= 100) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>Publish game</h3>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsGame()">Crunch!</button>'
+		mechanicsBlock.appendChild(tr);
+	}
+}
+
 // Turn mechanic
 function nextTurn() {
 	checkGameState();
 	player.ttl = player.ttl - 1;
 	updatePlayerStats();
+	renderMechanics(); // some mechanics display conditionally based on player stats, so we refresh every turn
 }
 
 // Win / Lose conditions
@@ -55,7 +128,6 @@ function checkGameState() {
 	else if (player.investor == 1) {
 		endGame("win");
 	}
-
 }
 
 function endGame(status) {
@@ -155,4 +227,5 @@ function eventsSprint() {
 
 window.onload = function () {
 	updatePlayerStats();
+	renderMechanics();
 }
