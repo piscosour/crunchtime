9abc8b86e935ceb1f42ebf4149efd4dcc07caaef
eduardo <piscosour@gmail.com>
2013-09-02 22:47:34 -0400

    Work on client mechanic

--- js/crunchtime.js
+++ js/crunchtime.js
@@ -18,6 +18,21 @@ function PlayerClass(name) {
 	this.investor = 0;
 	this.luck = 20;
 	this.stress = 50;
+	this.stressRate = 1;
+
+	this.client = false;
+
+	this.newClient = newClient;
+	function newClient() {
+		this.client = true;
+		this.clientProject = 0;
+	}
+
+	this.updateClientProject = updateClientProject;
+	function updateClientProject() {
+		this.clientProject = this.clientProject + Math.floor((Math.random()*player.capacity)+1);
+		nextTurn();
+	}
 }
 
 var playerName = window.prompt("Enter player name");
@@ -31,15 +46,22 @@ var player = new PlayerClass(playerName);
 function updatePlayerStats() {
 	player.cash = player.cash - player.burnrate;
 	player.ttl = player.cash / player.burnrate;
+	player.stress = player.stress + player.stressRate;
 }
 
 // update player stats display
 function renderPlayerStats() {
 	document.getElementById("playerNameBox").innerHTML = player.name;
 	document.getElementById("ttlBox").innerHTML = player.ttl + "days";
-	document.getElementById("completionBox").innerHTML = player.completion + "%";
+	if (player.client == false) {
+		document.getElementById("completionBox").innerHTML = player.completion + "%";
+	}
+	if (player.client == true) {
+		document.getElementById("completionBox").innerHTML = player.clientProject + "%";
+	}
 	document.getElementById("cashBox").innerHTML = "$" + player.cash;
 	document.getElementById("burnrateBox").innerHTML = "$" + player.burnrate;
+	document.getElementById("stressBox").innerHTML = player.stress + "%";
 }
 
 // mechanic text description
@@ -75,6 +97,10 @@ var mechanicTextsEn = [
 	{
 		"title": "Publish game",
 		"description": "Your prototype is complete! You're now ready to publish your game."
+	},
+	{
+		"title": "Work on client project",
+		"description": "You got a client! Now you have to deliver their project..."
 	}
 ]
 
@@ -99,12 +125,14 @@ function renderMechanics() {
 	mechanicsBlock.innerHTML = "";
 
 	// to do: move all mechanics to a JSON File
-	var tr = document.createElement("tr");
-	var td = tr.appendChild(document.createElement("td"));
-	var td2 = tr.appendChild(document.createElement("td"));
-	td.innerHTML = '<h3>' + mechanicTexts[0].title + '</h3>' + '<p>' + mechanicTexts[0].description + '</p>';
-	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsDevelop()">Crunch!</button>';
-	mechanicsBlock.appendChild(tr);
+	if (player.client == false) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>' + mechanicTexts[0].title + '</h3>' + '<p>' + mechanicTexts[0].description + '</p>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsDevelop()">Crunch!</button>';
+		mechanicsBlock.appendChild(tr);
+	}
 
 	var tr = document.createElement("tr");
 	var td = tr.appendChild(document.createElement("td"));
@@ -113,12 +141,23 @@ function renderMechanics() {
 	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsHire()">Crunch!</button>';
 	mechanicsBlock.appendChild(tr);
 
-	var tr = document.createElement("tr");
-	var td = tr.appendChild(document.createElement("td"));
-	var td2 = tr.appendChild(document.createElement("td"));
-	td.innerHTML = '<h3>' + mechanicTexts[2].title + '</h3>' + '<p>' + mechanicTexts[2].description + '</p>';
-	td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsClient()">Crunch!</button>';
-	mechanicsBlock.appendChild(tr);
+	if (player.client == false) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>' + mechanicTexts[2].title + '</h3>' + '<p>' + mechanicTexts[2].description + '</p>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="mechanicsClient()">Crunch!</button>';
+		mechanicsBlock.appendChild(tr);
+	}
+
+	if (player.client == true) {
+		var tr = document.createElement("tr");
+		var td = tr.appendChild(document.createElement("td"));
+		var td2 = tr.appendChild(document.createElement("td"));
+		td.innerHTML = '<h3>' + mechanicTexts[8].title + '</h3>' + '<p>' + mechanicTexts[8].description + '</p>';
+		td2.innerHTML = '<button class="btn btn-large btn-info" onclick="player.updateClientProject()">Crunch!</button>';
+		mechanicsBlock.appendChild(tr);	
+	}
 
 	var tr = document.createElement("tr");
 	var td = tr.appendChild(document.createElement("td"));
@@ -185,8 +224,17 @@ function rollDice() {
 // Turn mechanic
 function nextTurn() {
 	checkGameState();
-	rollDice();
+	// rollDice();
 	updatePlayerStats();
+	if (player.stress == 100) {
+		eventsSplit();
+	}
+	if (player.client == true) {
+		if (player.clientProject >= 100) {
+			window.alert("Congratulations! You finished your client project. Maybe now you can get back to your game...");
+			player.client = false;
+		}
+	}
 	renderPlayerStats();
 	renderMechanics(); // some mechanics display conditionally based on player stats, so we refresh every turn
 }
@@ -230,12 +278,15 @@ function mechanicsDevelop() {
 
 // recruit talent
 function mechanicsHire() {
-	player.skill = player.skill + 2;
+	player.skill = player.skill * 2;
+	player.cash = player.cash - 400;
 	nextTurn();
 }
 
 // find client work
 function mechanicsClient() {
+	player.newClient();
+	player.cash = player.cash + 2000;
 	nextTurn();
 }
 
@@ -283,24 +334,29 @@ function eventsLoan() {
 	window.alert("Your partner needs to borrow money from the company because of an emergency. You're not happy about this.");
 	player.cash = player.cash - 10;
 	player.stress = player.stress + 20;
+	player.stressRate = player.stressRate + 1;
 }
 
 // founder evicted (increases stress, reduces time)
 function eventsEviction() {
 	window.alert("Your partner couldn't make rent and has to move in with you. You now see each other all day. And you had to help him move his stuff.");
 	player.ttl = player.ttl - 3;
+	player.stress = player.stress + 20;
+	player.stressRate = player.stressRate + 1;
 }
 
 // unexpected tax bill
 function eventsTaxes() {
 	window.alert("You forgot to pay your taxes. You just got hit with a fine for failing to pay on time.");
 	player.cash = player.cash - 200;
+	player.stress = player.stress + 10;
 }
 
 // code sprint (increases completion)
 function eventsSprint() {
 	window.alert("You spent all night coding and managed to get ahead on the to do list. Coffee FTW.");
 	player.completion = player.completion + 10;
+	player.stress = player.stress + 10;
 }
 
 
